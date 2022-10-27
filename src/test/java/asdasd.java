9aea2cd4e3fd8d261b2493d329de6ff1dd101158
Mazlum Yuksel <mazlumyuksel2121@gmail.com>
2022-10-26 18:56:59 -0700

    commit1

--- src/test/java/asdasd.java
+++ src/test/java/asdasd.java
@@ -1,5 +1,9 @@
 public class asdasd {
     public static void main(String[] args) {
-        System.out.println("The new class");
+        System.out.println("The new clasdfsf" +
+                "sdfsdfs" +
+                "dfsdf" +
+                "sdf" +
+                "sdfass");
     }
 }
